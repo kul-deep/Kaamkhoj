2e50be9c08d4579495b06c99564675586b25dc51
Kuldeep Choudhary <kuldeepchoudhary501@gmail.com>
2020-05-13 00:53:36 +0530

    Login validation

--- lib/loginresgiter/Login.dart
+++ lib/loginresgiter/Login.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
-import 'package:kaamkhoj/afterlogin/dropdown.dart';
-import 'package:kaamkhoj/homepage.dart';
-import 'package:kaamkhoj/loginresgiter/forgetpassword.dart';
+// import 'package:kaamkhoj/homepage.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -20,8 +18,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
   String phoneNo,password,type;
-  final databaseReference = Firestore.instance;
+  // final databaseReference = Firestore.instance;
 
   String errorMessage = '';
 
@@ -29,6 +28,8 @@ class _LoginPageState extends State<LoginPage> {
     this.type=type;
     print("Login as "+type);
   }
+
+
 
 
   void verify() {
@@ -39,39 +40,34 @@ class _LoginPageState extends State<LoginPage> {
 //      snapshot.documents.forEach((f) => print('${f.data}}'));
 //    });
 
-    DocumentReference documentReference =
-    databaseReference.collection(type).document(phoneNo);
-    documentReference.get().then((datasnapshot) {
-      if (datasnapshot.exists) {
-        print(datasnapshot.data['password'].toString());
-        if(password==datasnapshot.data['password'].toString()){
-          print("Right Password");
-          SignIn();
-        }
-        else{
-          errorMessage="Wrong Credentials";
-          print("Wrong credentials");
-        }
-      }
-      else {
-        print("No such user");
-      }
-    });
+    // DocumentReference documentReference =
+    // databaseReference.collection(type).document(phoneNo);
+    // documentReference.get().then((datasnapshot) {
+    //   if (datasnapshot.exists) {
+    //     print(datasnapshot.data['password'].toString());
+    //     if(password==datasnapshot.data['password'].toString()){
+    //       print("Right Password");
+    //       SignIn();
+    //     }
+    //     else{
+    //       errorMessage="Wrong Credentials";
+    //       print("Wrong credentials");
+    //     }
+    //   }
+    //   else {
+    //     print("No such user");
+    //   }
+    // });
   }
 
 
   void SignIn(){
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //       builder: (context) => HomePage()),
+    // );
 
-    if(type=="Employer") {
-      Navigator.pushReplacement(context, MaterialPageRoute(
-          builder: (context) => ChooseYourWork("Employer",phoneNo)
-      ));
-    }
-    else {
-      Navigator.pushReplacement(context, MaterialPageRoute(
-          builder: (context) => ChooseYourWork("Employee",phoneNo)
-      ));
-    }
   }
 
   @override
@@ -79,84 +75,85 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-             Padding(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                    labelText: 'Mobile No.',
-                    hintText: '10 Digits'
-                ),
-                onChanged: (value) {
-                  this.phoneNo = "+91"+value;
-                },
-              ),
-            ),
-            (errorMessage != ''
-                ? Text(
-              errorMessage,
-              style: TextStyle(color: Colors.red),
-            )
-                : Container()),
-            SizedBox(
-              height: 10,
-            ), Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Password (minimum 6 digits)'),
-                onChanged: (value) {
-                  this.password = value;
-                },
-              ),
-            ),
-            (errorMessage != ''
-                ? Text(
-              errorMessage,
-              style: TextStyle(color: Colors.red),
-            )
-                : Container()),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.fromLTRB(16, 5, 0,5),
-              child:
-                new GestureDetector(
-
-                  onTap:(){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ForgetPassword(type,phoneNo),
-                      )
-                    );
+        child: Form(
+          key: formKey,
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+               Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(                  
+                  keyboardType: TextInputType.phone,                  
+                  decoration: InputDecoration(
+                      labelText: 'Mobile Number',
+                      hintText: 'Enter 10 digit number'),
+                  onChanged: (value) {
+                    this.phoneNo = "+91"+value;
                   },
-                  child: Text("Forget Password",
-
-                    style: new TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.blue,
-                    ),
-                  ),
+                   validator: (String value){
+                    if(value.length<10){
+                      return 'Mobile contains 10 digits';
+                    }
+              
+                  
+                },
+                onSaved: (String value){
+                         print("+91"+value);
+                      },
                 ),
-
-            ),
-            RaisedButton(
-              onPressed: () {
-                verify();
-              },
-              child: Text('Verify'),
-              textColor: Colors.white,
-              elevation: 7,
-              color: Colors.blue,
-            )
-          ],
+              ),
+              (errorMessage != ''
+                  ? Text(
+                errorMessage,
+                style: TextStyle(color: Colors.red),
+              )
+                  : Container()),
+              SizedBox(
+                height: 10,
+              ), Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'minimum 6 digits'),
+                  onChanged: (value) {
+                    this.password = value;
+                  },
+                  validator: (String value){
+                    if(value.length<6){
+                      return 'Password must contain atleast 6 characters';
+                    }
+                  
+                },
+                onSaved: (String value){
+                         print(value);
+                          },
+                ),
+              ),
+              (errorMessage != ''
+                  ? Text(
+                errorMessage,
+                style: TextStyle(color: Colors.red),
+              )
+                  : Container()),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                   if (formKey.currentState.validate()){
+                        formKey.currentState.save();
+                    }
+                  // verify();
+                },
+                child: Text('Verify'),
+                textColor: Colors.white,
+                elevation: 7,
+                color: Colors.blue,
+              )
+            ],
+          ),
         ),
       ),
     );
